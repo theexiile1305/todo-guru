81859e719c1342ad6a16f815b7e1ee32ce3fd790
akfxhll <arta97@hotmail.de>
2020-05-17 18:03:33 +0200

    layout create task

--- app/src/main/java/edu/hm/cs/ma/todoguru/SecondFragment.kt
+++ app/src/main/java/edu/hm/cs/ma/todoguru/SecondFragment.kt
@@ -1,12 +1,14 @@
 package edu.hm.cs.ma.todoguru
 
 import android.os.Bundle
+import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
-import androidx.fragment.app.Fragment
+import android.widget.EditText
 import androidx.navigation.fragment.findNavController
+import edu.hm.cs.ma.todoguru.database.Task
 
 /**
  * A simple [Fragment] subclass as the second destination in the navigation.
@@ -14,18 +16,24 @@ import androidx.navigation.fragment.findNavController
 class SecondFragment : Fragment() {
 
     override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
+        inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        (activity as MainActivity).supportActionBar?.title = getString(R.string.addTask)
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_second, container, false)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        val title = view.findViewById<EditText>(R.id.title).text
+        val description = view.findViewById<EditText>(R.id.description).text
+        val dueDate = view.findViewById<EditText>(R.id.dueDate).text
+        val estimated = view.findViewById<EditText>(R.id.estimated).text
 
-        view.findViewById<Button>(R.id.button_second).setOnClickListener {
+        Task(0L, title.toString(), description.toString(), dueDate.toString(), estimated.toString(), "")
+
+        view.findViewById<Button>(R.id.button_create).setOnClickListener {
             findNavController().navigate(R.id.action_SecondFragment_to_FirstFragment)
         }
     }
--- app/src/main/java/edu/hm/cs/ma/todoguru/database/Task.kt
+++ app/src/main/java/edu/hm/cs/ma/todoguru/database/Task.kt
@@ -1,10 +1,21 @@
 package edu.hm.cs.ma.todoguru.database
 
+import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 
 @Entity(tableName = "task_table")
 data class Task(
     @PrimaryKey(autoGenerate = true)
-    var id: Long = 0L
+    var id: Long = 0L,
+    @ColumnInfo(name = "title")
+    var title: String,
+    @ColumnInfo(name = "description")
+    var description: String,
+    @ColumnInfo(name = "due Date")
+    var dueDate: String,
+    @ColumnInfo(name = "estimated Time")
+    var estimated: String,
+    @ColumnInfo(name = "reminder")
+    var reminder: String
 )
