42feb6a5f28f6056c1e35aa88c06e194e684e09a
antonadamsson <56542651+antonadamsson@users.noreply.github.com>
2020-06-12 14:34:10 +0200

    Changed the description of entering estimated time

--- app/src/main/java/edu/hm/cs/ma/todoguru/MainActivity.kt
+++ app/src/main/java/edu/hm/cs/ma/todoguru/MainActivity.kt
@@ -5,9 +5,9 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import edu.hm.cs.ma.todoguru.notification.ReminderNotification
 
-class MainActivity: AppCompatActivity(){
+class MainActivity : AppCompatActivity(){
 
-    override fun onCreate(savedInstanceState:Bundle?){
+    override fun onCreate(savedInstanceState:Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         startService(Intent(this,ReminderNotification::class.java))
