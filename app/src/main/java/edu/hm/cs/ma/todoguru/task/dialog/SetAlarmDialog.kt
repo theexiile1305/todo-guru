42c6667bba5f03fd1317738f776ea1114fb225a4
Michael Fuchs <michael.fuchs@hm.edu>
2020-06-17 19:16:01 +0200

    Feature/#18 Set an alarm (#80)
    * added alarm dialog with required permission
    
    * added required icons
    
    * connecting alarm dialog to list of tasks
    
    * refactoring
    
    * refactoring
    
    * changed to alarm clock

---
+++ app/src/main/java/edu/hm/cs/ma/todoguru/task/dialog/SetAlarmDialog.kt
@@ -0,0 +1,38 @@
+package edu.hm.cs.ma.todoguru.task.dialog
+
+import android.app.TimePickerDialog
+import android.app.TimePickerDialog.OnTimeSetListener
+import android.content.Context
+import android.content.DialogInterface
+import android.content.Intent
+import android.provider.AlarmClock
+import edu.hm.cs.ma.todoguru.R
+import java.time.LocalDate
+import java.time.LocalTime
+import java.time.ZoneId
+
+class SetAlarmDialog(
+    context: Context,
+    private val intent: Intent = Intent(AlarmClock.ACTION_SET_ALARM).apply {
+        putExtra(AlarmClock.EXTRA_MESSAGE, context.getString(R.string.set_alarm))
+        putExtra(AlarmClock.EXTRA_SKIP_UI, true)
+    }
+) : TimePickerDialog(
+    context,
+    OnTimeSetListener { _, hourOfDay, minute ->
+        val currentDay = LocalDate.now().dayOfWeek.value
+        intent.putExtra(AlarmClock.EXTRA_DAYS, currentDay)
+        intent.putExtra(AlarmClock.EXTRA_HOUR, hourOfDay)
+        intent.putExtra(AlarmClock.EXTRA_MINUTES, minute)
+    },
+    LocalTime.now(ZoneId.of("Europe/Berlin")).hour,
+    LocalTime.now(ZoneId.of("Europe/Berlin")).minute,
+    true
+) {
+    override fun onClick(dialog: DialogInterface?, which: Int) {
+        super.onClick(dialog, which)
+        if (which == BUTTON_POSITIVE) {
+            context.startActivity(intent)
+        }
+    }
+}
