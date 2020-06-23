6e9f91f94a819be144be6d1dad5212d5bbe18100
antonadamsson <56542651+antonadamsson@users.noreply.github.com>
2020-06-23 20:21:45 +0200

    Added cat and subtasks

--- app/src/main/java/edu/hm/cs/ma/todoguru/task/read/ViewTaskFragment.kt
+++ app/src/main/java/edu/hm/cs/ma/todoguru/task/read/ViewTaskFragment.kt
@@ -7,9 +7,7 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.navArgs
 import edu.hm.cs.ma.todoguru.R
-import kotlinx.android.synthetic.main.view_task_fragment.description_task
-import kotlinx.android.synthetic.main.view_task_fragment.dueDate_task
-import kotlinx.android.synthetic.main.view_task_fragment.title_task
+import kotlinx.android.synthetic.main.view_task_fragment.*
 
 class ViewTaskFragment : Fragment() {
     private val args: ViewTaskFragmentArgs by navArgs()
@@ -31,5 +29,8 @@ class ViewTaskFragment : Fragment() {
         title_task.text = task.title
         description_task.text = task.description
         dueDate_task.text = task.dueDate.toString()
+        reminder_date.text = task.reminder.toString()
+        category.text = task.category
+        sub_task_list.text = task.subTask.toString()
     }
 }
