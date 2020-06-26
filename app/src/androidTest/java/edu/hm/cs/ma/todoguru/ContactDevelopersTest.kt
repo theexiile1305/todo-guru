13cb73a1754148a802a4f11ac77664efc7c41e33
theexiile1305 <michael.fuchs@hm.edu>
2020-06-26 16:29:00 +0200

    added tests

---
+++ app/src/androidTest/java/edu/hm/cs/ma/todoguru/ContactDevelopersTest.kt
@@ -0,0 +1,75 @@
+package edu.hm.cs.ma.todoguru
+
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
+import androidx.test.espresso.matcher.ViewMatchers.withContentDescription
+import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.espresso.matcher.ViewMatchers.withText
+import androidx.test.ext.junit.rules.ActivityScenarioRule
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.filters.LargeTest
+import org.hamcrest.Matchers.allOf
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@LargeTest
+@RunWith(AndroidJUnit4::class)
+class ContactDevelopersTest {
+
+    @Rule
+    @JvmField
+    var mActivityTestRule = ActivityScenarioRule(MainActivity::class.java)
+
+    @Test
+    fun contactDevelopersTest() {
+        skipExplanationDialogs()
+
+        val overflowMenuButton = onView(
+            allOf(
+                withContentDescription("More options"),
+                childAtPosition(
+                    childAtPosition(
+                        withId(R.id.topAppBar),
+                        1
+                    ),
+                    2
+                ),
+                isDisplayed()
+            )
+        )
+        overflowMenuButton.perform(click())
+
+        val textView = onView(
+            allOf(
+                withId(R.id.title), withText("Contact Developers"),
+                childAtPosition(
+                    childAtPosition(
+                        withId(R.id.content),
+                        0
+                    ),
+                    0
+                ),
+                isDisplayed()
+            )
+        )
+        textView.check(matches(isDisplayed()))
+
+        val materialTextView = onView(
+            allOf(
+                withId(R.id.title), withText("Contact Developers"),
+                childAtPosition(
+                    childAtPosition(
+                        withId(R.id.content),
+                        0
+                    ),
+                    0
+                ),
+                isDisplayed()
+            )
+        )
+        materialTextView.perform(click())
+    }
+}
